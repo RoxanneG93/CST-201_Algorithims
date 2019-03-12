4c2d9d71f1d580d27a14f0727a301952af5aa5ec
Roxanne Garcia <rxrgeneration@gmail.com>
2019-03-12 10:59:17 -0500

    maze attempt

---
+++ .vscode/Week1_Assignment/Maze5/Maze.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+// #include "olcConsoleGameEngine.h";
+
+class Maze()
+{
+
+    int width;
+    int height;
+    int *maze;
+
+    // A class that defines constants
+    enum
+    {
+        cell_path_N = 0x01,
+        cell_path_E = 0X02,
+        cell_path_S = 0X04,
+        cell_path_W = 0x08,
+        cell_VISITED = 0x10,
+
+    };
+};
+
+int main()
+{
+
+    return 0;
+}
